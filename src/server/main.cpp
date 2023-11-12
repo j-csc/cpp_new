3eee6089e2a28277f3b23401ed3b13e82370a302
Jason <chisanch@usc.edu>
2023-11-12 14:21:22 -0500

    nice

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <fmt/format.h>
+#include <core_lib.h>
 
 int main(int argc, char **argv)
 {
     fmt::print("Hello, {}!\n", "world");
+
+    core_lib();
     return 0;
 }
